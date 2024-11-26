7da13fef9789ce7c1e3493e1988c0603abd68242
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-11-26 19:37:28 +0530

    changes

--- client/src/protect/ProtectedHome.jsx
+++ client/src/protect/ProtectedHome.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
  function ProtecHome({children}){
-    
+
     const user = useSelector((state) => state.user.users)
     if(!user){
         return <Navigate to={"/"}/>
