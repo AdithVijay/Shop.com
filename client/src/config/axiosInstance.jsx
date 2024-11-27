b30d5df75f57c15d76806fa2c2a4cc2051a00600
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-11-27 18:11:32 +0530

    api url updated

--- client/src/config/axiosInstance.jsx
+++ client/src/config/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://v78xgw3pel.execute-api.ap-south-1.amazonaws.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json', 
