afe351a944c15f605b457c7572bf6ae4b52f6222
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-11-27 12:41:34 +0530

    changes

--- client/src/shared/banner/HeroSection.jsx
+++ client/src/shared/banner/HeroSection.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { ChevronLeft, ChevronRight, ShoppingBag } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-import banner1 from "../../assets/banner1.webp"
+import banner1 from "../../assets/banner1.avif"
 import banner2 from "../../assets/banner2.webp"
-import banner4 from "../../assets/banner4.png"
+import banner4 from "../../assets/banner4.avif"
 const HeroSection = () => {
   const images = [
     {
